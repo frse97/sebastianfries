423e062d119abc4a6cc5d446707181fb49e02063
frse97 <sebastianfries@outlook.com>
2022-04-28 01:12:58 +0200

    Improved front, right, back faces.

--- src/components/FrseCube/Faces/Back.tsx
+++ src/components/FrseCube/Faces/Back.tsx
@@ -1,8 +1,77 @@
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import "./Faces.scss";
+import {
+  faDiscord,
+  faGithub,
+  faInstagram,
+  faLinkedin,
+  faTwitter,
+  faWhatsapp,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Back: React.FC = () => {
-  return <div className="back-content">Back</div>;
+  return (
+    <div className="back-content">
+      <div className="top-row">Contact me</div>
+      <div className="contacts-row">
+        <div className="entry">
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="mailto:sebastianfries@outlook.com"
+          >
+            <FontAwesomeIcon icon={faEnvelope} />
+          </a>
+        </div>
+        <div className="entry">
+          <a target="_blank" rel="noreferrer" href="tel:+436606400786">
+            <FontAwesomeIcon icon={faWhatsapp} />
+          </a>
+        </div>
+        <div className="entry">
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/sebastian-fries-7321a01a3/"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+        </div>
+      </div>
+      <div className="contacts-row">
+        <div className="entry">
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.instagram.com/essidbrot/"
+          >
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
+        </div>
+        <div className="entry">
+          <a target="_blank" rel="noreferrer" href="https://twitter.com/Frse97">
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
+        </div>
+        <div className="entry">
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://discord.com/channels/frse97#1789"
+          >
+            <FontAwesomeIcon icon={faDiscord} />
+          </a>
+        </div>
+        <div className="entry">
+          <a target="_blank" rel="noreferrer" href="https://github.com/frse97">
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Back;
--- src/components/FrseCube/Faces/Right.tsx
+++ src/components/FrseCube/Faces/Right.tsx
@@ -1,8 +1,79 @@
+import {
+  faApple,
+  faCss3,
+  faFigma,
+  faHtml5,
+  faJs,
+  faLess,
+  faReact,
+  faSass,
+  faVuejs,
+  faWindows,
+} from "@fortawesome/free-brands-svg-icons";
+import { faStar, faStarHalfStroke } from "@fortawesome/free-solid-svg-icons";
+import { faStar as faStarOutlined } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import "./Faces.scss";
 
 const Right: React.FC = () => {
-  return <div className="right-content">Right</div>;
+  return (
+    <div className="right-content">
+      <div className="top-row">Technology Stack</div>
+      <div className="entry">
+        <FontAwesomeIcon icon={faReact} />
+        <span>React</span>
+        <div className="star-container">
+          <FontAwesomeIcon icon={faStar} />
+          <FontAwesomeIcon icon={faStar} />
+          <FontAwesomeIcon icon={faStar} />
+          <FontAwesomeIcon icon={faStarHalfStroke} />
+          <FontAwesomeIcon icon={faStarOutlined} />
+        </div>
+      </div>
+      <div className="entry">
+        <FontAwesomeIcon icon={faVuejs} />
+        <span>Vue</span>
+        <div className="star-container">
+          <FontAwesomeIcon icon={faStar} />
+          <FontAwesomeIcon icon={faStar} />
+          <FontAwesomeIcon icon={faStarOutlined} />
+          <FontAwesomeIcon icon={faStarOutlined} />
+          <FontAwesomeIcon icon={faStarOutlined} />
+        </div>
+      </div>
+      <div className="entry">
+        <FontAwesomeIcon icon={faSass} />
+        <span>Sass</span>
+        <div className="star-container">
+          <FontAwesomeIcon icon={faStar} />
+          <FontAwesomeIcon icon={faStar} />
+          <FontAwesomeIcon icon={faStar} />
+          <FontAwesomeIcon icon={faStar} />
+          <FontAwesomeIcon icon={faStarOutlined} />
+        </div>
+      </div>
+      <div className="entry">
+        <FontAwesomeIcon icon={faLess} />
+        <span>Less</span>
+        <div className="star-container">
+          <FontAwesomeIcon icon={faStar} />
+          <FontAwesomeIcon icon={faStar} />
+          <FontAwesomeIcon icon={faStar} />
+          <FontAwesomeIcon icon={faStar} />
+          <FontAwesomeIcon icon={faStarOutlined} />
+        </div>
+      </div>
+      <div className="entry-other">
+        <FontAwesomeIcon icon={faWindows} />
+        <FontAwesomeIcon icon={faApple} />
+        <FontAwesomeIcon icon={faHtml5} />
+        <FontAwesomeIcon icon={faCss3} />
+        <FontAwesomeIcon icon={faJs} />
+        <FontAwesomeIcon icon={faFigma} />
+      </div>
+    </div>
+  );
 };
 
 export default Right;
